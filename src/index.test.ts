96e1069942027b3d43bdb6bc5d68517a56906df3
yemreak <yemreak@icloud.com>
2024-05-05 11:55:24 +0300

    init: aws s3 upload file

---
+++ src/index.test.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from "fs"
+import yaml from "js-yaml"
+import { S3Client } from "./index.js"
+
+describe("S3Client", () => {
+	let s3Client: S3Client
+	const content = readFileSync("credentials.yml", "utf8")
+	const init = yaml.load(content) as {
+		bucketName: string
+		region: string
+		credentials: { accessKeyId: string; secretAccessKey: string }
+	}
+
+	beforeEach(() => {
+		s3Client = new S3Client(init)
+	})
+
+	describe("put", () => {
+		it("should upload a file to S3", async () => {
+			const response = await s3Client.put({
+				filepath: "tsconfig.json",
+				isPublic: true,
+			})
+
+			expect(response).toContain(init.bucketName)
+			expect(response).toContain(init.region)
+		})
+	})
+
+	describe("constructUrl", () => {
+		it("should construct a valid S3 URL", () => {
+			const url = s3Client.constructUrl("testFile")
+
+			expect(url).toBe(
+				`https://${init.bucketName}.s3.${init.region}.amazonaws.com/testFile`
+			)
+		})
+	})
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
-async function main() {
-	console.log("Hello npm package!")
-}
+import {
+	PutObjectCommand,
+	S3Client as _S3Client,
+	type S3ClientConfig,
+} from "@aws-sdk/client-s3"
+import fs from "fs"
+import mime from "mime-types"
+export { type S3ClientConfig }
 
-main()
+export class S3Client {
+	private readonly client: _S3Client
+	readonly bucketName: string
+	readonly region: string
+
+	constructor(init: {
+		bucketName: string
+		region: string
+		credentials: S3ClientConfig["credentials"]
+	}) {
+		this.bucketName = init.bucketName
+		this.region = init.region
+
+		this.client = new _S3Client({ ...init })
+	}
+
+	async put(params: { filepath: string; isPublic: boolean }) {
+		const { filepath, isPublic = false } = params
+
+		const file = fs.readFileSync(filepath)
+		const filename = filepath.split("/").pop()!
+		const mimeType = mime.lookup(filepath) || "application/octet-stream"
+		const key = `${Date.now()}_${filename}`
+		const command = new PutObjectCommand({
+			Bucket: this.bucketName,
+			Key: key,
+			Body: file,
+			ContentType: mimeType,
+			ACL: isPublic ? "public-read" : "private",
+		})
+
+		const response = await this.client.send(command)
+		if (!response.$metadata.httpStatusCode) {
+			throw new Error("Failed to upload file to S3.")
+		}
+
+		return this.constructUrl(key)
+	}
+
+	constructUrl(filename: string): string {
+		return `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${filename}`
+	}
+}
